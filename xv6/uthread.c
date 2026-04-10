aa9fa9198ea3cecf5ac2774f6c9a2b1dbf661614
SetWarrantyBit <kywjoseph@gmail.com>
2026-04-09 22:26:00 -0500

    .

--- xv6/uthread.c
+++ xv6/uthread.c
@@ -8,10 +8,10 @@
 
 // TODO: Implement cooperative user-level threads.
 
-void thread_init(void){}
-tid_t thread_create(void (*fn)(void*), void *arg){ (void)fn; (void)arg; return -1; }
-void thread_yield(void){}
-int thread_join(tid_t tid){ (void)tid; return -1; }
+//void thread_init(void){}
+//tid_t thread_create(void (*fn)(void*), void *arg){ (void)fn; (void)arg; return -1; }
+//void thread_yield(void){}
+//int thread_join(tid_t tid){ (void)tid; return -1; }
 
 enum tstate {FREE, RUNNABLE, RUNNING, ZOMBIE};
 
